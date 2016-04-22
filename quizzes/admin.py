2064c8afb1d6d7dbae78a47c8550fa7ccf4923dc
Mike Jones <mike@westerncapelabs.com>
2016-04-22 17:09:45 +0200

    Add quiz to questions relationship

--- quizzes/admin.py
+++ quizzes/admin.py
@@ -2,5 +2,22 @@ from django.contrib import admin
 
 from .models import Quiz, Question
 
-admin.site.register(Quiz)
-admin.site.register(Question)
+
+class QuizAdmin(admin.ModelAdmin):
+    list_display = [
+        "id", "description", "active",
+        "created_at", "updated_at", "created_by", "updated_by"]
+    list_filter = ["active", "created_at"]
+    search_fields = ["description"]
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = [
+        "id", "version", "question_type", "question",
+        "response_correct", "response_incorrect", "active",
+        "created_at", "updated_at", "created_by", "updated_by"]
+    list_filter = ["active", "question_type", "created_at"]
+    search_fields = ["description", "question", "answers"]
+
+admin.site.register(Quiz, QuizAdmin)
+admin.site.register(Question, QuestionAdmin)
--- quizzes/models.py
+++ quizzes/models.py
@@ -7,45 +7,6 @@ from django.utils.encoding import python_2_unicode_compatible
 
 
 @python_2_unicode_compatible
-class Quiz(models.Model):
-
-    """
-    Base Quiz model, does not hold questions
-    """
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    description = models.CharField(max_length=200)
-    active = models.BooleanField(default=False)
-    metadata = JSONField(null=True, blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    created_by = models.ForeignKey(User, related_name='quizzes_created',
-                                   null=True)
-    updated_by = models.ForeignKey(User, related_name='quizzes_updated',
-                                   null=True)
-    user = property(lambda self: self.created_by)
-
-    def serialize_hook(self, hook):
-        # optional, there are serialization defaults
-        # we recommend always sending the Hook
-        # metadata along for the ride as well
-        return {
-            'hook': hook.dict(),
-            'data': {
-                'id': str(self.id),
-                'description': self.description,
-                'metadata': self.metadata,
-                'created_at': self.created_at.isoformat(),
-                'created_by': self.created_by.username,
-                'updated_at': self.updated_at.isoformat(),
-                'updated_by': self.updated_by.username
-            }
-        }
-
-    def __str__(self):  # __unicode__ on Python 2
-        return str(self.id)
-
-
-@python_2_unicode_compatible
 class Question(models.Model):
     QUESTION_TYPE_CHOICES = (
         ('multiplechoice', "Multiple Choice"),
@@ -87,6 +48,46 @@ class Question(models.Model):
                 'response_correct': self.response_correct,
                 'response_incorrect': self.response_incorrect,
                 'active': self.active,
+                'created_at': self.created_at.isoformat(),
+                'created_by': self.created_by.username,
+                'updated_at': self.updated_at.isoformat(),
+                'updated_by': self.updated_by.username
+            }
+        }
+
+    def __str__(self):  # __unicode__ on Python 2
+        return str(self.id)
+
+
+@python_2_unicode_compatible
+class Quiz(models.Model):
+
+    """
+    Base Quiz model, does not hold questions
+    """
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    description = models.CharField(max_length=200)
+    active = models.BooleanField(default=False)
+    metadata = JSONField(null=True, blank=True)
+    questions = models.ManyToManyField(Question, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    created_by = models.ForeignKey(User, related_name='quizzes_created',
+                                   null=True)
+    updated_by = models.ForeignKey(User, related_name='quizzes_updated',
+                                   null=True)
+    user = property(lambda self: self.created_by)
+
+    def serialize_hook(self, hook):
+        # optional, there are serialization defaults
+        # we recommend always sending the Hook
+        # metadata along for the ride as well
+        return {
+            'hook': hook.dict(),
+            'data': {
+                'id': str(self.id),
+                'description': self.description,
+                'metadata': self.metadata,
                 'created_at': self.created_at.isoformat(),
                 'created_by': self.created_by.username,
                 'updated_at': self.updated_at.isoformat(),
