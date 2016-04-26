b762e8086bc202405a7c80ea1ed6d07ce5201ec1
Mike Jones <mike@westerncapelabs.com>
2016-04-26 21:29:10 +0200

    Updated admin

--- quizzes/admin.py
+++ quizzes/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Quiz, Question
+from .models import Quiz, Question, Answer, Tracker
 
 
 class QuizAdmin(admin.ModelAdmin):
@@ -19,5 +19,25 @@ class QuestionAdmin(admin.ModelAdmin):
     list_filter = ["active", "question_type", "created_at"]
     search_fields = ["description", "question", "answers"]
 
+
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = [
+        "id", "version", "question", "question_text", "answer_value",
+        "answer_text", "answer_correct", "response_sent", "tracker",
+        "created_at", "created_by", "updated_at", "updated_by"]
+    list_filter = ["answer_correct", "question", "created_at"]
+    search_fields = ["question_text", "answer_text"]
+
+
+class TrackerAdmin(admin.ModelAdmin):
+    list_display = [
+        "id", "identity", "quiz", "complete",
+        "started_at", "created_by", "completed_at", "updated_by"]
+    list_filter = ["complete", "started_at", "completed_at"]
+    search_fields = ["identity"]
+
+
 admin.site.register(Quiz, QuizAdmin)
 admin.site.register(Question, QuestionAdmin)
+admin.site.register(Answer, AnswerAdmin)
+admin.site.register(Tracker, TrackerAdmin)
