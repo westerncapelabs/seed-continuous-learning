27a1640e92c78b13b59e2caed4284add052f49ab
Mike Jones <mike@westerncapelabs.com>
2016-06-16 01:25:42 +0200

    smaller page size

--- seed_continuous_learning/settings.py
+++ seed_continuous_learning/settings.py
@@ -118,7 +118,7 @@ RAVEN_CONFIG = {
 # REST Framework conf defaults
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAdminUser',),
-    'PAGE_SIZE': 1000,
+    'PAGE_SIZE': 100,
     'DEFAULT_PAGINATION_CLASS':
         'rest_framework.pagination.LimitOffsetPagination',
     'DEFAULT_AUTHENTICATION_CLASSES': (
